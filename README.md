b2b879e9ffe7a822ab816533e3c43a89099bd192
yemiwebby <yemiwebby@gmail.com>
2023-06-23 14:13:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+[![CircleCI](https://circleci.com/gh/CIRCLECI-GWP/my-laravel-api-tutorial/tree/main.svg?style=svg)](https://circleci.com/gh/CIRCLECI-GWP/my-laravel-api-tutorial/tree/main)
+
+<p align="center"><img src="https://avatars3.githubusercontent.com/u/59034516"></p>
+
+## Details
+
+This repo is built following a tutorial published on CircleCI blog under the CircleCI Guest Writer Program.
+
+- Blog post: [Building a CI Powered RESTful API with Laravel][blog]
+- Author's GitHub profile: [Fikayo Adepoju][author]
+
+### About CircleCI Guest Writer Program
+
+Join a team of freelance writers and write about your favorite technology topics for the CircleCI blog. Read more about the program [here][gwp-program].
+
+[blog]: https://circleci.com/blog/build-a-ci-powered-restful-api-with-laravel/
+[author]: https://github.com/coderonfleek
+
+[gwp-program]: https://circle.ci/3ahQxfu
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    use WithFaker;
+
+    private $password = "mypassword";
+
+    public function testUserCreation()
+    {
+
+        $name = $this->faker->name();
+        $email = $this->faker->email();
+
+        $response = $this->postJson('/api/auth/signup', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $this->password,
+            'password_confirmation' => $this->password
+        ]);
+
+
+        $response
+            ->assertStatus(201)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
+    }
+
+    public function testUserLogin()
+    {
+        $name = $this->faker->name();
+        $email = $this->faker->email();
+
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($this->password)
+        ]);
+
+        $user->save();
+
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $email,
+            'password' => $this->password
+        ]);
+
+
+        $response->assertStatus(200);
+        $this->assertAuthenticated();
+    }
+}
